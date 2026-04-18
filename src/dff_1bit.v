e5c2e0681966babbf28d46c3d8dc661e8d980016
Dori <dorina.vlonga@gmail.com>
2026-04-18 23:24:31 +0300

    1 bit register

--- src/dff_1bit.v
+++ src/dff_1bit.v
@@ -1,17 +1,20 @@
 `timescale 1ns / 1ps
 
 module dff_1bit (
-    input  wire clk,
-    input  wire reset,
-    input  wire en,
-    input  wire d,
-    output reg  q
+    input  wire clk,   // Semnal de ceas
+    input  wire reset, // Reset asincron
+    input  wire en,    // Semnal de activare (Enable)
+    input  wire d,     // Intrarea de date (1 bit)
+    output reg  q      // Ieșirea bistabilului
 );
 
+    // Logică secvențială pentru stocarea unui bit
     always @(posedge clk or posedge reset) begin
         if (reset == 1'b1) begin
+            // Resetare ieșire la 0
             q <= 1'b0;
         end else if (en == 1'b1) begin
+            // Dacă este activat, preia valoarea de la intrare
             q <= d;
         end
     end
