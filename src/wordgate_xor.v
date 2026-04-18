40447a4d9ef5da57fdd077d52dc2a24e8921ed3c
Dori <dorina.vlonga@gmail.com>
2026-04-18 23:28:23 +0300

    wordgate implementation

--- src/wordgate_xor.v
+++ src/wordgate_xor.v
@@ -1,16 +1,17 @@
 `timescale 1ns / 1ps
 
 module wordgate_xor (
-    input  wire [8:0] in_data,
-    input  wire       C6,
-    output reg  [8:0] out_data
+    input  wire [8:0] in_data,  // Datele de intrare (9 biți)
+    input  wire       C6,       // Semnal de control pentru inversare
+    output reg  [8:0] out_data  // Datele de ieșire
 );
 
+    // Logică combinațională pentru complementare (inversare biți)
     always @(*) begin
         if (C6 == 1'b1) begin
-            out_data = ~in_data;
+            out_data = ~in_data; // Inversare (folosită la scădere)
         end else begin
-            out_data = in_data;
+            out_data = in_data;  // Datele trec neschimbate
         end
     end
 
