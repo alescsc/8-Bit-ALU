5d9f3136394e0d1e545b71ab4d0b4e0e47320183
alescsc <76198547+alescsc@users.noreply.github.com>
2026-04-18 23:31:33 +0300

    ALU logic

--- src/ALU.v
+++ src/ALU.v
@@ -1,19 +1,22 @@
 `timescale 1ns / 1ps
 
 module ALU (
-    input  wire       clk,
-    input  wire       reset,
-    input  wire       begin_sig,
-    input  wire [1:0] OP_MODE,
-    input  wire [7:0] INBUS,
-    output wire [7:0] OUTBUS
+    input  wire       clk,        // Ceas
+    input  wire       reset,      // Reset sistem
+    input  wire       begin_sig,  // Start operatie
+    input  wire [1:0] OP_MODE,    // Selectie operatie (00:+, 01:-, 10:*, 11:/)
+    input  wire [7:0] INBUS,      // Intrare date
+    output wire [7:0] OUTBUS      // Iesire rezultat
 );
 
+    // Semnale de control interne (generate de CU)
     wire C0, C1, C2, C3, C4, C5, C6, C7, C8, C9;
     wire C10, C11, C12, C13, C14, C15, C16, C17, C18;
     
+    // Semnale de stare (trimise catre CU)
     wire Q1_out, Q0_out, R_out, OVR_out, A7_out, COUNT7;
 
+    // Componenta pentru registre si calcule
     datapath DP (
         .clk(clk), .reset(reset), .INBUS(INBUS),
         .C0(C0), .C1(C1), .C2(C2), .C3(C3), .C4(C4), 
@@ -25,6 +28,7 @@ module ALU (
         .OUTBUS(OUTBUS)
     );
 
+    // Componenta pentru automatul de stari
     control_unit CU (
         .clk(clk), .reset(reset), .begin_sig(begin_sig), .OP_MODE(OP_MODE),
         .Q0(Q0_out), .R(R_out), .OVR(OVR_out), .A7(A7_out), .COUNT7(COUNT7),
@@ -32,5 +36,3 @@ module ALU (
         .C7(C7), .C8(C8), .C9(C9), .C10(C10), .C11(C11), .C12(C12), 
         .C13(C13), .C14(C14), .C15(C15), .C16(C16), .C17(C17), .C18(C18)
     );
-
-endmodule
